159f541b94cc2032de16d3ac2bf31af52836b5cc
devinaarimbifitriani <devinaarimbi43@gmail.com>
2024-09-26 20:51:59 +0700

    Add files via upload

---
+++ Devina Arimbi Fitriani.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main ()
+{ 
+    printf("Devina Arimbi Fitriani");
+    printf("NIM 2441010");
+    printf("Teknik Informatika");
+    return 0;
+}
---
+++ latihan 5.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+	printf("Halo STT Mandala");
+	return 0;
+}
